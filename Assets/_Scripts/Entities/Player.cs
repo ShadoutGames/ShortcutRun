49b5ef5a391ce54d9bd975b2f9833d834aecec19
keremtpl <keremtpl57@gmail.com>
2022-05-16 16:13:36 +0300

    throwedWood Added

--- Assets/_Scripts/Entities/Player.cs
+++ Assets/_Scripts/Entities/Player.cs
@@ -12,31 +12,36 @@ namespace NoName
         [SerializeField]
         private float speed;
 
-		[SerializeField]
-		private float turnSpeed;
+        [SerializeField]
+        private float turnSpeed;
 
         #endregion
 
         #region Variables
 
-		private Rigidbody rb;
-		private Transform _transform;
+        private Rigidbody rb;
+        private Transform _transform;
         private float distanceTravelled;
         private float currentSpeed;
+        private float time;
 
         #endregion
 
         #region Props
 
         #endregion
+        #region Actions
+        public event Action WayOut;
+
+        #endregion
 
         #region Unity Methods
 
-		private void Awake() 
-		{
-			rb = GetComponent<Rigidbody>();
-			_transform = transform;
-		}
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+            _transform = transform;
+        }
 
         private void Start()
         {
@@ -47,23 +52,25 @@ namespace NoName
 
         private void Update()
         {
-			rb.velocity = _transform.forward * speed;
+            time -= Time.deltaTime;
+            rb.velocity = _transform.forward * speed;
 
-			var closestDistance = PathManager.Instance.Path.path.GetClosestPointOnPath(transform.position);
-			if(Vector3.Distance(transform.position, closestDistance) > PathManager.Instance.PathMesh.roadWidth)
-			{
-				Debug.Log("asd");
-			}
+            var closestDistance = PathManager.Instance.Path.path.GetClosestPointOnPath(transform.position);
+            if (Vector3.Distance(transform.position, closestDistance) > PathManager.Instance.PathMesh.roadWidth)
+            {
+                Debug.Log("asd");
+                WayOut?.Invoke();
+            }
         }
 
         private void OnTriggerEnter(Collider other)
         {
-	        var collectWood = other.GetComponent<ICollectable>();
-	        if (collectWood!=null)
-	        {
-		        CollectManager.Instance.Add();
-		        other.gameObject.SetActive(false);
-	        }
+            var collectWood = other.GetComponent<ICollectable>();
+            if (collectWood != null)
+            {
+                CollectManager.Instance.Add();
+                other.gameObject.SetActive(false);
+            }
         }
 
         #endregion
--- Assets/_Scripts/Managers/CollectManager.cs
+++ Assets/_Scripts/Managers/CollectManager.cs
@@ -5,88 +5,109 @@ using UnityEngine;
 
 namespace NoName
 {
-public class CollectManager : Singleton<CollectManager>
-{
-	#region SerializedFields
-
-	[SerializeField]
-	private GameObject woodPrefab;
-
-	[SerializeField]
-	private int maxWoodCount;
-
-	[SerializeField]
-	private Transform parentWoods;
-
-	[SerializeField]
-	private float offsetY;
-
-	#endregion
-
-	#region Variables
-	
-	private GameObject[] woods;
-
-	private int currentWoodCount = 0;
-
-
-	#endregion
-
-	#region Props
-
-	public int CurrentWoodCount => currentWoodCount;
-
-	#endregion
-
-	#region Unity Methods
-
-	private void Start()
-	{
-		woods = new GameObject[maxWoodCount];
-
-		for (int i = 0; i < maxWoodCount; i++)
-		{
-			var wood = Instantiate(woodPrefab, new Vector3(parentWoods.position.x, parentWoods.position.y + i * offsetY, parentWoods.position.z), Quaternion.identity, parentWoods);
-			woods[i] = wood;
-			wood.SetActive(false);
-		}
-
-		for (int i = 0; i < currentWoodCount; i++)
-		{
-			woods[i].SetActive(true);
-		}
-	}
-
-	private void Update() 
-	{
-		if(currentWoodCount == 1 && currentWoodCount == 0) return;
-
-		for (int i = 1; i < currentWoodCount; i++)
-		{
-			woods[i].transform.localPosition = Vector3.Lerp(woods[i].transform.localPosition, woods[i - 1].transform.localPosition + offsetY * Vector3.up, (currentWoodCount - i) *.03f);
-		}
-	}
-
-	#endregion
-
-	#region Methods
-	
-	public void Add()
+    public class CollectManager : Singleton<CollectManager>
     {
-        woods[currentWoodCount].SetActive(true);
-		currentWoodCount++;
-    }
-	
-    public void Remove()
-    {
-        woods[currentWoodCount-1].SetActive(false);
-		currentWoodCount--;
-    }
-    
-    #endregion
+        #region SerializedFields
 
-	#region Callbacks
+        [SerializeField]
+        private GameObject woodPrefab;
 
-	#endregion
-}
+        [SerializeField]
+        private int maxWoodCount;
+
+        [SerializeField]
+        private Transform parentWoods;
+
+        [SerializeField]
+        private float offsetY;
+
+        #endregion
+
+        #region Variables
+
+        private GameObject[] woods;
+        private GameObject[] throwedWoods;
+
+        private int currentWoodCount = 0;
+
+
+        #endregion
+
+        #region Props
+
+        public int CurrentWoodCount => currentWoodCount;
+
+        #endregion
+
+        #region Unity Methods
+
+        private void Start()
+        {
+            Player.Instance.WayOut += OnWayOut;
+            woods = new GameObject[maxWoodCount];
+            throwedWoods = new GameObject[maxWoodCount];
+
+            for (int i = 0; i < maxWoodCount; i++)
+            {
+                var wood = Instantiate(woodPrefab, new Vector3(parentWoods.position.x, parentWoods.position.y + i * offsetY, parentWoods.position.z), Quaternion.identity, parentWoods);
+                var throwed = Instantiate(woodPrefab, new Vector3(parentWoods.position.x, parentWoods.position.y + i * offsetY, parentWoods.position.z), Quaternion.identity);
+                woods[i] = wood;
+                throwedWoods[i] = throwed;
+                throwed.SetActive(false);
+                wood.SetActive(false);
+
+            }
+
+            for (int i = 0; i < currentWoodCount; i++)
+            {
+                woods[i].SetActive(true);
+            }
+
+        }
+
+
+        private void Update()
+        {
+            if (currentWoodCount == 1 && currentWoodCount == 0) return;
+
+            for (int i = 1; i < currentWoodCount; i++)
+            {
+                woods[i].transform.localPosition = Vector3.Lerp(woods[i].transform.localPosition, woods[i - 1].transform.localPosition + offsetY * Vector3.up, (currentWoodCount - i) * .03f);
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Add()
+        {
+            woods[currentWoodCount].SetActive(true);
+            currentWoodCount++;
+        }
+
+        public void Remove()
+        {
+            woods[currentWoodCount - 1].SetActive(false);
+            currentWoodCount--;
+        }
+
+        #endregion
+
+        #region Callbacks
+        private void OnWayOut()
+        {
+            if (currentWoodCount > 0)
+            {
+                throwedWoods[currentWoodCount - 1].transform.position = new Vector3(Player.Instance.transform.position.x,
+                0,
+                Player.Instance.transform.position.z);
+                throwedWoods[currentWoodCount - 1].SetActive(true);
+                Remove();
+            }
+
+        }
+
+        #endregion
+    }
 }
